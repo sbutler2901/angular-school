aaf0a9fe7b82da345704e36a29a83697cc329df9
sbutler2901 <sbsoftware@protonmail.com>
2018-04-07 17:19:34 -0400

    The topics service now successfully communicates with the server app to handle topic data some aspects still need implementing. Added documentation to files currently working.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('school App', () => {
+describe('angular-school App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/services/topic.service.ts
+++ src/app/services/topic.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import {TOPICS} from '../mock-topics';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import {Topic} from '../topic';
 import {Course} from '../course';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable() // tells Angular that this service might itself have injected dependencies.
 export class TopicService {
@@ -13,45 +16,60 @@ export class TopicService {
   private topics: Topic[];
   private topicsURL = 'http://localhost:8081/topics';  // URL to web api
 
-  constructor(
-    private http: HttpClient,
-  ) {
+  constructor( private http: HttpClient ) { }
 
-    // this.topics = TOPICS;
-  }
-
-  // Provides asynchronous retrieval of data
+  /**
+   * Get all topics from the server
+   * @return - observable of topics being retrieved
+   */
   getTopics(): Observable<Topic[]> {
-    // return of(this.topics);    // returns an Observable<Topic[]> that emits a single value, the array of mock heroes.
-
-    this.topics = [];
-    /** GET topics from the server */
-    return this.http.get<Topic[]>(this.topicsURL)
-      .pipe(
-        tap(topics => ( this.log(`fetched topics`) ),
+    return this.http.get<Topic[]>(this.topicsURL).pipe(
+        tap(() => ( this.log(`fetched topics`) )),
         catchError(this.handleError('getTopics', []))
       );
   }
-  getTopic(id: string): Observable<Topic> {
-    // Todo: send the message _after_ fetching the hero
-    // this.messageService.add(`HeroService: fetched hero id=${id}`);
-    // return of(this.topics.find(topic => topic.id === id));
 
+  /**
+   * Get a specific topic from the server
+   * @param id - the id of the topic to be retrieved
+   * @return - observable of topic being retrieved
+   */
+  getTopic(id: string): Observable<Topic> {
     const url = `${this.topicsURL}/${id}`;
+
     return this.http.get<Topic>(url).pipe(
-      tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
+      tap(() => this.log(`fetched topic id=${id}`)),
+      catchError(this.handleError<Topic>(`getTopic id=${id}`))
     );
   }
+
+  /**
+   * Delete a topic from the server
+   * @param id - the id of the topic to be deleted
+   */
   deleteTopic(id: string): void {
     const index: number = this.topics.findIndex(topic => topic.id === id);
     this.topics.splice(index, 1);
   }
-  addTopic(topic: Topic): void {
-    //this.topics.push(topic);
 
+  /**
+   * Add a new topic to the server.
+   * @param topic - the topic to be added
+   * @return - observable of topic being added
+   */
+  addTopic(topic: Topic): Observable<Topic> {
+    console.log('Adding tpic ');
+    return this.http.post<Topic>(this.topicsURL, topic, httpOptions).pipe(
+      tap((ntopic: Topic) => this.log(`added topic w/ id=${ntopic.id}`)),
+      catchError(this.handleError<Topic>('addTopic'))
+    );
   }
 
+  /**
+   * Add a course under a specific topic to the server
+   * @param topicID - the id of the topic containing the course
+   * @param course - the course to be added
+   */
   addCourse(topicID: string, course: Course) {
     const topicx: Topic = this.topics.find(topic => topic.id === topicID);
     topicx.courses.push(course);
@@ -77,9 +95,12 @@ export class TopicService {
     };
   }
 
-  /** Log a HeroService message with the MessageService */
+  /**
+   * Log a TopicService message with the MessageService
+   * @param message - message to be logged
+   */
   private log(message: string) {
     console.log('TopicService: ' + message);
-    //this.messageService.add('HeroService: ' + message);
+    // this.messageService.add('TopicService: ' + message);
   }
 }
